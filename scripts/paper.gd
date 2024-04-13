2c69fb1291d235a6e99c8e4c63f48934fe220862
CharmedSatyr <16056963+CharmedSatyr@users.noreply.github.com>
2024-04-13 06:57:10 -0700

    Use constant drag speed

--- scripts/paper.gd
+++ scripts/paper.gd
@@ -1,8 +1,10 @@
 class_name Paper
 extends CharacterBody2D
 
+const DRAG_SPEED: Vector2 = Vector2(30, 30)
+
 var draggingDistance: float
-var dir: Vector2
+var direction: Vector2
 var dragging: bool
 var newPosition: Vector2 = Vector2()
 
@@ -13,21 +15,20 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if is_selected and event.is_pressed() && mouse_in:
 			draggingDistance = position.distance_to(get_viewport().get_mouse_position())
-			dir = (get_viewport().get_mouse_position() - position).normalized()
+			direction = (get_viewport().get_mouse_position() - position).normalized()
 			dragging = true
-			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
+			newPosition = get_viewport().get_mouse_position() - draggingDistance * direction
 		else:
 			dragging = false
 			is_selected = false
 
 	elif event is InputEventMouseMotion:
 		if dragging:
-			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
+			newPosition = get_viewport().get_mouse_position() - draggingDistance * direction
 
 func _physics_process(_delta: float) -> void:
 	if dragging:
-		# TODO: what are these numbers 30, 30?
-		set_velocity((newPosition - position) * Vector2(30, 30))
+		set_velocity((newPosition - position) * DRAG_SPEED)
 		move_and_slide()
 
 func select() -> void:
