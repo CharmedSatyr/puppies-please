76ea6c6f039618d88b1df091d2db532805572515
CharmedSatyr <16056963+CharmedSatyr@users.noreply.github.com>
2024-04-15 08:13:03 -0700

    reorganize. update puppy photos to better size

--- scripts/data/puppies.gd
+++ scripts/data/puppies.gd
@@ -2,7 +2,7 @@ class_name Puppies
 extends Node
 
 const Database: Array[Dictionary] = [
-	{ "name" : "Theodore", "color": "brown", "size": "medium", "energy": "low", "is_capitalist": "false", "sprite": "fly" },
+	{ "name" : "Theodore", "color": "brown", "size": "medium", "energy": "low", "is_capitalist": "false", "sprite": "fly", "image": "" },
 	{ "name" : "Mr. Squiggles", "color": "blue", "size": "tiny", "energy": "medium", "is_capitalist": "false", "sprite": "walk" },
 	{ "name" : "Ace", "color": "blonde", "size": "luggage", "energy": "rocketship", "is_capitalist": "false", "sprite": "swim" },
 	{ "name" : "Milo", "color": "black & white", "size": "teeny tiny", "energy": "a lot", "is_capitalist": "true", "sprite": "player" }
