90961b74ad53942a2cb5656d6385e16e62d2fe4d
CharmedSatyr <16056963+CharmedSatyr@users.noreply.github.com>
2024-04-15 10:36:03 -0700

    Puppy updates

--- scripts/data/puppies.gd
+++ scripts/data/puppies.gd
@@ -2,8 +2,8 @@ class_name Puppies
 extends Node
 
 const Database: Array[Dictionary] = [
-	{ "name" : "Theodore", "color": "brown", "size": "medium", "energy": "low", "is_capitalist": "false", "sprite": "theo", "image": "theodore.png" },
-	{ "name" : "Mr. Squiggles", "color": "blue", "size": "tiny", "energy": "medium", "is_capitalist": "false", "sprite": "mr-squiggles", "image": "mr-squiggles.png" },
-	{ "name" : "Ace", "color": "blonde", "size": "luggage", "energy": "rocketship", "is_capitalist": "false", "sprite": "ace", "image": "ace.png" },
-	{ "name" : "Milo", "color": "black & white", "size": "teeny tiny", "energy": "a lot", "is_capitalist": "true", "sprite": "milo", "image": "milo.png" }
+	{ "name" : "Theodore", "color": "Brown", "size": "Small", "energy": "Chill", "is_capitalist": "false", "sprite": "theo", "image": "theodore.png" },
+	{ "name" : "Mr. Squiggles", "color": "Black", "size": "Little", "energy": "Bouncy Ball", "is_capitalist": "false", "sprite": "mr-squiggles", "image": "mr-squiggles.png" },
+	{ "name" : "Ace", "color": "Spotted", "size": "Medium", "energy": "Rocketship", "is_capitalist": "false", "sprite": "ace", "image": "ace.png" },
+	{ "name" : "Milo", "color": "White & Brown", "size": "Small", "energy": "Racecar", "is_capitalist": "true", "sprite": "milo", "image": "milo.png" }
 ]
